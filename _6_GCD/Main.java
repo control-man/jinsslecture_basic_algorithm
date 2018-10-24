ccf8ad3a8e4046d986bb2190d1d2ab0105baf9f1
pjh2174 <jinhyuk2174@hanmail.net>
2018-10-24 23:17:51 +0900

    gcd

---
+++ _6_GCD/Main.java
@@ -0,0 +1,32 @@
+package _6_GCD;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int num1, num2;
+		num1 = 30;
+		num2 = 20;
+		
+		int small;
+		int big;
+		
+		if (num1 > num2){
+			big = num1;
+			small = num2;
+		}
+		else {
+			big = num2;
+			small = num1;
+		}
+		
+		int gcd = 1; //최대공약수
+		
+		for (int i = 1 ; i <= small  ; i++) {
+			if (big % i == 0 && small % i == 0) 
+				gcd = i;
+		}
+		System.out.println(gcd);
+	}
+
+}
