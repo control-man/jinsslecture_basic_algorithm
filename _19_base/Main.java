b333cf244c6f316109b5b69387f615d800e9791b
pjh2174 <jinhyuk2174@hanmail.net>
2018-11-15 22:34:36 +0900

    base

---
+++ _19_base/Main.java
@@ -0,0 +1,14 @@
+package _19_base;
+
+public class Main {
+	public static void main(String[] args) {
+		int n = 5;
+		int m = 3;
+		int j = 1;
+		
+		for (int i = 0 ; i < m ; i++) {
+			j *= n;
+		}
+		System.out.println(j);
+	}
+}
